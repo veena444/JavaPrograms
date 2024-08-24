6ce8f66a0d58928de243faad51f1f251ef8575e8
Veena Hegde <veena444@gmail.com>
2024-08-24 16:25:38 +0530

    First commit

---
+++ JavaPrograms/src/javaPrograms/Print1To100WithoutUsingNumbers.java
@@ -0,0 +1,23 @@
+package javaPrograms;
+/*
+ * Print from 1 to 100 without using any numbers in your code
+ */
+public class Print1To100WithoutUsingNumbers {
+
+	public static void main(String[] args) {
+		int one ='a'/'a';//1
+		String str = "..........";
+		
+		for(int i=one;i<=(str.length() * (str.length()));i++) {
+			System.out.println(i);
+		}
+		
+		//a-97,b-98,c-99,d-100
+		for(int i=one;i<='d';i+=one) {
+			System.out.println(i);
+		}
+
+	}
+
+}
+
