8bd3922fc51ca7c47247211b7c4d46a75496b7e1
Veena Hegde <veena444@gmail.com>
2024-12-29 15:09:04 +0530

    Added array is sorted or not

---
+++ src/array/ArrayIsSorted.java
@@ -0,0 +1,26 @@
+package array;
+/*
+ * Find the given array is sorted or not.
+ */
+public class ArrayIsSorted {
+
+	public static void main(String[] args) {
+		int[] nums = {10};
+		int n = nums.length;
+		System.out.println(ifSorted(nums,n));
+
+	}
+	
+	public static boolean ifSorted(int[] nums, int n) {
+		if(n == 0 || n == 1) {
+			return true;
+		}
+		for(int i=1;i<n;i++) {
+			if(nums[i-1] > nums[i]) {
+				return false;
+			}			
+		}
+		return true;
+	}
+
+}
