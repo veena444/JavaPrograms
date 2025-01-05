4a299cc0500df97f678f00b4eb409d6a5760f6af
Veena Hegde <veena444@gmail.com>
2025-01-05 16:28:21 +0530

    Added remove duplicate elements

---
+++ src/array/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+package array;
+
+public class RemoveDuplicates {
+
+	public static void main(String[] args) {
+		int[] arr = {0,0,1,1,1,2,2,3,3,4};
+		System.out.println(removeDuplicates(arr));
+
+	}
+	
+	public static int removeDuplicates(int[] arr) {
+		int count = 0;
+		for(int i=0;i<arr.length;i++) {
+			if(i < arr.length-1 && arr[i] == arr[i+1]) {
+				continue;
+			}
+			else {
+				arr[count] = arr[i];
+				count++;
+			}
+		}
+		return count;
+	}
+
+}
