ff624036c46474b3513711306b4dfc3f694844c1
Veena Hegde <veena444@gmail.com>
2025-02-16 18:12:33 +0530

    Added sort the array using bubble sort

---
+++ src/arraySorting/UsingLoops_BubbleSort.java
@@ -0,0 +1,29 @@
+package arraySorting;
+
+import java.util.Arrays;
+
+public class UsingLoops_BubbleSort {
+
+	public static void main(String[] args) {
+		int[] arr = {5,6,3,1,8,4,0,2,7,0};
+		arraySort(arr);
+		System.out.println(Arrays.toString(arr));
+		
+		
+
+	}
+	
+	public static void arraySort(int[] arr) {
+		for(int i=0;i<arr.length-1;i++) {
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}	
+		
+	}
+
+}
