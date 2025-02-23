febe47ea36d996efd01be6761c47bc199163006d
Veena Hegde <veena444@gmail.com>
2025-02-23 12:18:53 +0530

    Sum of array

---
+++ src/interviewPrograms/SumOfArray.java
@@ -0,0 +1,20 @@
+package interviewPrograms;
+
+public class SumOfArray {
+
+	public static void main(String[] args) {
+		int[] arr = {10,20,30};
+		System.out.println(sumOfArray(arr));
+
+	}
+	
+	public static int sumOfArray(int[] arr) {
+		int sum = 0;
+		
+		for(int i=0;i<arr.length;i++) {
+			sum = sum + arr[i];
+		}
+		return sum;
+	}
+
+}
